cb5cedc146d194a50748507bf27d6625bf9db061
derpman23 <ianliaozz@gmail.com>
2022-02-04 23:16:19 -0500

    added gnu commands and finished compiler

--- acsl-asm/Main.cpp
+++ acsl-asm/Main.cpp
@@ -1,9 +1,25 @@
 #include "ArgumentParser.h"
 #include "Parser.h"
+#include "Util.h"
+
+#include<cstdio>
 
 int main(int argc, char** argv) {
 	arguments args = parse_args(argc, argv);
 	if (args.error) return 1;
 
 	auto c = get_c(args.input);
+
+	fs::path c_file = args.output_c ? args.c : fs::path(args.input.stem().string() + ".c");
+	write_file(c_file, c);
+
+	fs::path bin_file = args.output_bin ? args.bin : fs::path(args.input.stem().string() + ".exe");
+	string gnu = "g++ -o " + bin_file.string() + " " + c_file.string();
+	system(gnu.c_str());
+
+	if (!args.output_c) remove(c_file.string().c_str());
+
+	system(bin_file.string().c_str());
+
+	if (!args.output_bin) remove(bin_file.string().c_str());
 }
--- acsl-asm/Parser.cpp
+++ acsl-asm/Parser.cpp
@@ -37,7 +37,7 @@ string get_c(fs::path in) {
 	string code = "#include<stdio.h>\nint main(){int acc=0;";
 
 	if (fin.is_open()) {
-		string line;
+		string line, body = "";
 		std::set<string> defined;
 		while (std::getline(fin, line)) {
 			std::stringstream ss(line);
@@ -58,70 +58,68 @@ string get_c(fs::path in) {
 
 			switch (opcodes[fields[1]]) {
 			case 1:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "acc=" + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "acc=" + fields[2] + ";";
 				break;
 			case 2:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				if (defined.find(fields[2]) == defined.end()) {
-					code += "int ";
-					defined.insert(fields[2]);
-				}
-				code += fields[2] + "=acc;";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				defined.insert(fields[2]);
+				body += fields[2] + "=acc;";
 				break;
 			case 3:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "acc+=" + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "acc+=" + fields[2] + ";";
 				break;
 			case 4:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "acc-=" + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "acc-=" + fields[2] + ";";
 				break;
 			case 5:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "acc*=" + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "acc*=" + fields[2] + ";";
 				break;
 			case 6:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "acc/=" + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "acc/=" + fields[2] + ";";
 				break;
 			case 7:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "if(acc>0)goto " + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "if(acc>0)goto " + fields[2] + ";";
 				break;
 			case 8:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "if(!acc)goto " + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "if(!acc)goto " + fields[2] + ";";
 				break;
 			case 9:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "if(acc<0)goto " + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "if(acc<0)goto " + fields[2] + ";";
 				break;
 			case 10:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "goto " + fields[2] + ";";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "goto " + fields[2] + ";";
 				break;
 			case 11:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				if (defined.find(fields[2]) == defined.end()) {
-					code += "int " + fields[2] + ";";
-					defined.insert(fields[2]);
-				}
-				code += "scanf(\"%d\",&" + fields[2] + ");";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				defined.insert(fields[2]);
+				body += "scanf(\"%d\",&" + fields[2] + ");";
 				break;
 			case 12:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "printf(\"%d\\n\"," + fields[2] + ");";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "printf(\"%d\\n\"," + fields[2] + ");";
 				break;
 			case 13:
-				code += "int " + fields[0] + "=" + fields[2] + ";";
+				defined.insert(fields[0]);
+				body += fields[0] + "=" + fields[2] + ";";
 				break;
 			case 14:
-				if (!fields[0].empty()) code += fields[0] + ":";
-				code += "return 0;";
+				if (!fields[0].empty()) body += fields[0] + ":";
+				body += "return 0;";
 				break;
 			}
 		}
+		for (string s : defined)
+			code += "int " + s + ";";
+		code += body;
 	}
 
 	code += "}";
